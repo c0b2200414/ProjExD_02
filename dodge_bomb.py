5878b6dc6cab70f305ee05f8bfcbc669a4472dc7
asami <c0b2200414@edu.teu.ac.jp>
2023-04-25 17:42:38 +0900

    importする順番は、random,sys,pygameにするほうがコード規約に元図いていていいと思う　＃１－３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,10 +3,10 @@ import sys
 import random
 
 delta = {
-        pg.K_UP: (0, -1),
-        pg.K_DOWN: (0, +1),
-        pg.K_LEFT: (-1, 0),
-        pg.K_RIGHT: (+1, 0)
+        pg.K_UP: (0, -1),   # 上キーを押した時の反応
+        pg.K_DOWN: (0, +1), # 下キーを押した時の反応
+        pg.K_LEFT: (-1, 0), # 左キーを押した時の反応
+        pg.K_RIGHT: (+1, 0) # 右キーを押した時の反応
         }
 
 def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
@@ -34,7 +34,6 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
 
     kk_img_ks = pg.transform.flip(kk_img, True, True)
-
     kk_mk = {
             (0, -1):pg.transform.rotozoom(kk_img, 0,  1),
             (-1,-1):pg.transform.rotozoom(kk_img, 45, 1),
@@ -44,20 +43,19 @@ def main():
             (+1,+1):pg.transform.rotozoom(kk_img,180,1),
             (+1,0):pg.transform.rotozoom(kk_img,225,1),
             (+1,-1):pg.transform.rotozoom(kk_img,270,1),
-            } #演習課題未完
+            } # 演習課題未完
     
 
     bb_img = pg.Surface((20, 20))
     kk_rct = kk_img.get_rect()
-    kk_rct.center = 900, 400
-    z = 10
-    pg.draw.circle(bb_img, (255, 0, 0), [z,z], 10)
-    x, y = random.randint(0, 1600), random.randint(0, 900)
-    screen.blit(bb_img, [x, y])
+    kk_rct.center = 900, 400 # kk_imageの最初に現れる座標
+    pg.draw.circle(bb_img, (255, 0, 0), (10,10), 10)
+    x, y = random.randint(0, 1600), random.randint(0, 900) # x座標、y座標をランダムに表示させるためのコード
+    screen.blit(bb_img, [x, y]) # bb_imageをランダムに開始するコード
     bb_img.set_colorkey((0, 0, 0))
     bb_rct = bb_img.get_rect()
     bb_rct.center = x, y
-    vx, vy = +1, +1
+    vx, vy = +1, +1 # こうかとんの移動速度
     tmr = 0
 
     while True:
@@ -66,21 +64,18 @@ def main():
                 return 0
             
         tmr += 1
-        z *= 5
-        if z == 100:
-            z = 100
         key_lst = pg.key.get_pressed()
         for k, mv in delta.items():
             if key_lst[k]:
-                kk_rct.move_ip(mv)         
+                kk_rct.move_ip(mv)   # 移動キーを押したときの移動      
 
         if check_bound(screen.get_rect(), kk_rct) != (True, True):
             for k, mv in delta.items():
                 if key_lst[k]:
-                    kk_rct.move_ip(-mv[0], -mv[1])
+                    kk_rct.move_ip(-mv[0], -mv[1]) # こうかとんが範囲外に出ないようにする
 
         
-        screen.blit(bb_img, [0, 0])
+        screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(screen.get_rect(), bb_rct)
@@ -90,7 +85,7 @@ def main():
             vy *= -1
         screen.blit(bb_img, bb_rct)
 
-        if kk_rct.colliderect(bb_rct):
+        if kk_rct.colliderect(bb_rct): 
             return
         
 
